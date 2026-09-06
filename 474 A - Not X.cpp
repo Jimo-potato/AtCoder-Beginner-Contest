c20ff8955cc65e9d0a8dae4cb33af93c7c530a10
Jimo-potato <76jirou@gmail.com>
2026-09-06 21:05:43 +0900

    Create 474 A - Not X.cpp

---
+++ 474 A - Not X.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int X;
+  cin >> X;
+  
+  if (X == 1) {
+    cout << "2" << endl;
+  } else if (X == 2) {
+    cout << "3" << endl;
+  } else {
+    cout << "1" << endl;
+  }
+}
+/*
+correct answer(python)
+
+x = int(input())
+if x == 1:
+    print(2)
+else:
+    print(1)
+
+*/
