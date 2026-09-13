268eec2a9b5ab5e67c5ebc3603f054552896de98
Jimo-potato <76jirou@gmail.com>
2026-09-13 17:23:21 +0900

    475 A - mnclr.cpp

--- 475 A - mnclr.cpp
+++ 475 A - mnclr.cpp
@@ -10,3 +10,21 @@ int main() {
   }
   cout << a[n-1] << endl;
 }
+
+/*
+correct answer
+
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+  string s;
+  cin >> s;
+  int n=s.size();
+  string t(2*n-1,' ');
+  for(int i=0;i<n;i++)t[2*i]=s[i];
+  for(int i=1;i<n;i++)t[2*i-1]='o';
+  cout << t << endl;
+}
+
+*/
