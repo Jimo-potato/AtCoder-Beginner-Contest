2135b72a420f16327dff8285535d662254712fed
Jimo-potato <76jirou@gmail.com>
2026-09-19 22:49:15 +0900

    476 C - Third Largest Number.cpp

---
+++ 476 C - Third Largest Number.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  int N;
+  cin >> N;
+  vector<int> A(3);
+  for (int i = 0; i < 3; i++) {
+    cin >> A.at(i);
+  }
+  sort(A.rbegin(), A.rend());
+  cout << A.at(2) << endl;
+  int last = A.at(2);
+  A.pop_back();
+  
+  for (int i = 0; i < N - 3; i++) {
+    int Ak;
+    cin >> Ak;
+    if (Ak < last) {
+      A.push_back(last);
+    } else if (Ak <= A.at(1)) {
+      A.push_back(Ak);
+    } else if (Ak <= A.at(0)) {
+      A.insert(A.begin() + 1, Ak);
+    } else {
+      A.insert(A.begin(), Ak);
+    }
+    cout << A.at(2) << endl;
+    last = A.at(2);
+    A.pop_back();
+  }
+}
