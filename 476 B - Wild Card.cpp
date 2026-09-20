fefd8bbcab766c2eda245ca1d6c8234d6bf5f23b
Jimo-potato <76jirou@gmail.com>
2026-09-20 20:15:36 +0900

    476 B - Wild Card.cpp

--- 476 B - Wild Card.cpp
+++ 476 B - Wild Card.cpp
@@ -22,3 +22,21 @@ int main() {
     cout << "No" << endl;
   }
 }
+
+// correct answer
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int N;
+    cin>>N;
+    string S,T;
+    cin>>S>>T;
+    string ans="Yes";
+    for(int i=0;i<N;i++){
+        if(T[i]!=S[i]&&T[i]!='*'){
+            ans="No";
+        }
+    }
+    cout<<ans<<"\n";
+}
