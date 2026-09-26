df7cdbe903879b9208613953f7fcdd5b3ca6859e
Jimo-potato <76jirou@gmail.com>
2026-09-26 22:57:31 +0900

    477 A - Traffic Light.cpp

---
+++ 477 A - Traffic Light.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+  char c;
+  cin >> c;
+  if (c == 'B') {
+    cout << 'Y' << endl;
+  } else if (c == 'Y') {
+    cout << 'R' << endl;
+  } else {
+    cout << 'B' << endl;
+  }
+}
